8f18d1af2287ec5cf7512307747bcfe268c54d4b
Timothee Cour <timothee.cour2@gmail.com>
2019-02-13 13:12:23 -0800

    _

--- nimterop/compat.nim
+++ nimterop/compat.nim
@@ -14,22 +14,6 @@ else:
   import std/os
   export parentDir, DirSep
 
-when (NimMajor, NimMinor, NimPatch) >= (0, 19, 9):
-  export relativePath
-else:
-  proc relativePath*(file, base: string): string =
-    ## naive version of `os.relativePath` ; remove after nim >= 0.19.9
-    runnableExamples:
-      import ospaths, unittest
-      check:
-        "/foo/bar/baz/log.txt".unixToNativePath.relativePath("/foo/bar".unixToNativePath) == "baz/log.txt".unixToNativePath
-        "foo/bar/baz/log.txt".unixToNativePath.relativePath("foo/bar".unixToNativePath) == "baz/log.txt".unixToNativePath
-    var base = base.normalizedPath
-    var file = file.normalizedPath
-    if not base.endsWith DirSep: base.add DirSep
-    doAssert file.startsWith base
-    result = file[base.len .. ^1]
-
 when workaround_10629:
   const DirSep* = '\\'
   proc parentDirPos(path: string): int =
@@ -45,3 +29,20 @@ when workaround_10629:
       result = substr(path, 0, sepPos-1)
     else:
       result = ""
+
+
+when (NimMajor, NimMinor, NimPatch) >= (0, 19, 9):
+  export relativePath
+else:
+  proc relativePath*(file, base: string): string =
+    ## naive version of `os.relativePath` ; remove after nim >= 0.19.9
+    runnableExamples:
+      import ospaths, unittest
+      check:
+        "/foo/bar/baz/log.txt".unixToNativePath.relativePath("/foo/bar".unixToNativePath) == "baz/log.txt".unixToNativePath
+        "foo/bar/baz/log.txt".unixToNativePath.relativePath("foo/bar".unixToNativePath) == "baz/log.txt".unixToNativePath
+    var base = base.normalizedPath
+    var file = file.normalizedPath
+    if not base.endsWith DirSep: base.add DirSep
+    doAssert file.startsWith base
+    result = file[base.len .. ^1]
